9ace227cd4afa7a88c5019e99bf300ea62db4823
KZellweger <kevin.zellweger@students.fhnw.ch>
2021-01-11 22:32:24 +0100

    Render basic table for Upload

--- src/components/UploadComponent.jsx
+++ src/components/UploadComponent.jsx
@@ -1,7 +1,27 @@
-import {useState} from "react";
+import React, {forwardRef, useState} from "react";
 import axios from "axios";
+import {
+    AddBox,
+    ArrowDownward,
+    Check,
+    ChevronLeft,
+    ChevronRight,
+    Clear, Delete,
+    DeleteOutline,
+    Edit,
+    FilterList,
+    FirstPage,
+    LastPage,
+    Remove,
+    SaveAlt,
+    Search,
+    ViewColumn
+} from "@material-ui/icons";
+import Link from "@material-ui/core/Link";
+import MaterialTable from "material-table";
 
 const ACCOUNT_REST_API_URL = 'http://localhost:8081/upload/'
+const IMAGE_PREFIX = 'https://api.cardmarket.com'
 
 export default function UploadComponent() {
     const [card, setCard] = useState({
@@ -56,23 +76,92 @@ export default function UploadComponent() {
                 setProgress(progress);
             }
         }).then(res => {
-            console.log(res);
+            setCardList(res.data);
+            console.log(res.data)
+            console.log(cardList)
         }).catch(err => console.log(err))
     }
+
+    const handleEdit = () => {
+        //todo
+    }
+    const handleDelete = () => {
+        //todo
+    }
+
+    // Table options
+    const options = {
+        actionsColumnIndex: -1,
+        sorting: true
+    };
+
+    const actions = [{
+        icon: Edit,
+        tooltip: 'Edit Item',
+        onClick: (event, rowData) => handleEdit(rowData),
+    },
+        {
+            icon: Delete,
+            tooltip: 'Delete Item',
+            onClick: (event, rowData) => handleDelete(rowData)
+        }
+    ];
+
+
+    const columns = [
+        {title: "EN-Name", field: "name", render: rowData => {return <p>{rowData.name}</p>}},
+        {title: "Sprache", field: "language", render: rowData => {return <p>{rowData.language}</p>}},
+        {title: "Name", field: "productName", render: rowData => {return <p>{rowData.productName}</p>}},
+        {title: "Kategorie", field: "categoryName", render: rowData => {return <p>{rowData.categoryName}</p>}},
+        {title: "Bild", field: "imageurl", render: rowData => {return <img src={rowData.imageUrl.replace(".",IMAGE_PREFIX)} style={{width: 50, borderRadius: '10%'}} />}},
+        {title: "Rarity", field: "rarity", render: rowData => {return <p>{rowData.rarity}</p>}},
+        {title: "Set Title", field: "expansionName", render: rowData => {return <p>{rowData.expansionName}</p>}},
+        {title: "Hinzugefügt", field: "dateAdded", render: rowData => {return <p>{rowData.dateAdded}</p>}}
+        ]
+
+    const icons = {
+        Add: forwardRef((props, ref) => <AddBox {...props} ref={ref}/>),
+        Check: forwardRef((props, ref) => <Check {...props} ref={ref}/>),
+        Clear: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
+        Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref}/>),
+        DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
+        Edit: forwardRef((props, ref) => <Edit {...props} ref={ref}/>),
+        Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref}/>),
+        Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref}/>),
+        FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref}/>),
+        LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref}/>),
+        NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
+        PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref}/>),
+        ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
+        Search: forwardRef((props, ref) => <Search {...props} ref={ref}/>),
+        SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref}/>),
+        ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref}/>),
+        ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref}/>)
+    };
+
 
     return (
         <div>
             <div className="file-upload">
-                <input type="file" onChange={handleFileChange} />
-                <div className="bg-info" style={{ width: progress }}>
+                <input type="file" onChange={handleFileChange}/>
+                <div className="bg-info" style={{width: progress}}>
                     {progress}
                 </div>
                 <button onClick={uploadFile} className="bg-primary">
                     Upload
                 </button>
-                <hr />
+                <hr/>
             </div>
+            <MaterialTable
+                options={options}
+                columns={columns}
+                data={cardList}
+                icons={icons}
+                actions={actions}
+                title="Exhibition Items"
+            />
         </div>
+
     );
 
 }
